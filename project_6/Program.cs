5f5f4877dd699dc03a0f0212f7a17fb65b7c4fd9
Herlin Priatna <herlinpriatna@student.telkomuniversity.ac.id>
2023-03-31 09:08:28 +0700

    membuat parsing sederhana

--- project_6/Program.cs
+++ project_6/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Masukkan sebuah angka : ");
+        string inputString = Console.ReadLine();
+
+        try
+        {
+            int inputInt = int.Parse(inputString);
+            int inputInt2 = Convert.ToInt32(inputString);
+            int hasilKuadrat = checked(inputInt * inputInt);
+            Console.WriteLine("Hasil kuadrat dari " + inputInt2 + " adalah = " + hasilKuadrat);
+        } catch(FormatException e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine("Input tidak bisa dikonversi");
+        } catch (OverflowException e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine("Angka hasil kuadrat terlalu besar");
+        } catch (ArithmeticException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        
+    }
+}
